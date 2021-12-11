56cbeb01169a27e45ffc5d3e58ef5d3d4da9ebc7
jaimeperezsanchez <jaime.perez.sanchez@gmail.com>
2021-12-11 20:05:14 +0100

    Day 1

---
+++ 1_code.py
@@ -0,0 +1,14 @@
+# Read the input file 
+with open(r'C:\Users\jperezs\OneDrive - Universidad Pontificia Comillas\Documentos\Python Scripts\Advent_2021\1_input.txt', 'r') as f:
+    data = f.readlines()
+
+# Set counter to 0
+counter_increases = 0
+# Loop the data and check if the next number is bigger than the previous one
+pre_number = int(data[0])
+for n in data[1:]:
+    post_number = int(n)
+    if post_number > pre_number:
+        counter_increases += 1
+    pre_number = post_number
+print(counter_increases)
---
+++ 2_code.py
@@ -0,0 +1,14 @@
+# Read the input file 
+with open(r'C:\Users\jperezs\OneDrive - Universidad Pontificia Comillas\Documentos\Python Scripts\Advent_2021\2_input.txt', 'r') as f:
+    data = f.readlines()
+
+# Set counter to 0
+counter_increases = 0
+# Loop the data and check if the next three-sliding window is bigger than the previous
+pre_number_list = [int(n) for n in data[:3]]
+for i in range(1, len(data)-2):
+    post_number_list = [int(n) for n in data[i:i+3]]
+    if sum(post_number_list) > sum(pre_number_list):
+        counter_increases += 1
+    pre_number_list = post_number_list
+print(counter_increases)
