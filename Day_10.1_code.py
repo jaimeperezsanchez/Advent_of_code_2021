6c6130aa757aeb9ec807a49a9da0589ee275d86d
jaimeperezsanchez <jaime.perez.sanchez@gmail.com>
2021-12-20 20:49:51 +0100

    Day 10

---
+++ Day_10.1_code.py
@@ -0,0 +1,46 @@
+from functools import total_ordering
+
+
+with open('./Day_10_input.txt', 'r') as f:
+    data = f.readlines()
+
+def get_score(line):
+    stack = []
+    line_lst = list(line.strip())
+    for char in line_lst:
+        if char in open_chunk:
+            stack.append(char)
+        elif char in close_chunk:
+            open_char = open_chunk[close_chunk.index(char)]
+            if stack and stack[-1] == open_char:
+                stack.pop()
+            else:
+                if char == ')':
+                    return 3
+                elif char == ']':
+                    return 57
+                elif char == '}':
+                    return 1197
+                elif char == '>':
+                    return 25137
+    if stack:
+        return 0
+    else:
+        # Incomplete sequences
+        return 0
+
+## Scores
+# ): 3 points.
+# ]: 57 points.
+# }: 1197 points.
+# >: 25137 points
+
+def main():
+    total_score = sum(get_score(line) for line in data)
+    print(total_score)
+
+if __name__ == '__main__':
+    open_chunk = '([{<'
+    close_chunk = ')]}>'
+    main()
+    
