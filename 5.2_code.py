783c23e01e23e1d9262ebc4e8f9dbf29ccc1fcbd
jaimeperezsanchez <jaime.perez.sanchez@gmail.com>
2021-12-20 19:10:29 +0100

    Day 5

--- 5.2_code.py
+++ 5.2_code.py
@@ -1,4 +1,4 @@
-with open(r'C:\Users\jperezs\OneDrive - Universidad Pontificia Comillas\Documentos\Python Scripts\Advent_of_code_2021\5_input.txt', 'r') as f:
+with open('./5_input.txt', 'r') as f:
     data = f.readlines()
 
 def check_diagonal_Bottom_right(x1, y1, x2, y2):
